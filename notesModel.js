dae66cecb288d313977e1fb2a256118da25577e9
Sarah <dousse.sarah@gmail.com>
2022-01-10 15:41:35 +0000

    #5 TDD NotesModel addNote()

---
+++ notesModel.js
@@ -0,0 +1,15 @@
+class NotesModel {
+  constructor() {
+    this.notes = [];
+  }
+
+  getNotes() {
+    return this.notes;
+  }
+
+  addNote(note) {
+    this.notes.push(note);
+  }
+}
+
+module.exports = NotesModel;
---
+++ test/notesModel.test.js
@@ -0,0 +1,15 @@
+const NotesModel = require("../notesModel");
+
+describe("Notes App Class", () => {
+  const model = new NotesModel();
+  it("can be instantiated", () => {
+    expect(model.getNotes()).toEqual([]);
+  });
+
+  it("can add an item to the notes array", () => {
+    model.addNote("Buy milk");
+    model.addNote("Go to the gym");
+    expect(model.getNotes().length).toBe(2);
+    expect(model.getNotes()).toEqual(["Buy milk", "Go to the gym"]);
+  });
+});
